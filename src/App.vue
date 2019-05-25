2124577043c1493cca631455804f191a08b145a3
kravisingh135 <kravisingh135@gmail.com>
2019-05-25 22:46:41 +0530

    adding dist folder

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <h1>Real time stock data</h1>
+  <div class="head-content">
+    <div class="name">Name</div>
+    <div class="price">Price</div>
+    <div class="updated-at">Updated At</div>
+  </div>
+  <div class="body-content">        
+    <div v-for="message in messages" :key="message.value">
+      <ul>
+        <li v-for="messageList in message">
+          <div class="name">{{messageList.name}}</div>
+          <div class="price">{{messageList.price}}</div>
+          <div class="updated-at">{{messageList.updatedAt | moment}}</div>
+        </li>
+      </ul>
+    </div>    
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Socket from "./socket"
+    import moment from 'moment'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    export default {
+        name: 'app',
+        data(){
+            return {
+                messages: []                                
+            }
+        },
+        methods:{
+          handleMessage(msg){                       
+            this.messages.push(msg) 
+          }
+        },
+        filters: {
+          moment: function (date) {
+            return moment(date).fromNow();
+          }
+        },
+        created(){
+            Socket.$on("message", this.handleMessage)
+        },
+        beforeDestroy(){
+            Socket.$off("message", this.handleMessage)
+        }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/css/style.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Socket from './socket'
+import moment from 'moment'
+
+Vue.prototype.moment = moment
 
 Vue.config.productionTip = false
+
+// let count = 0
+
+setInterval(() => {
+  Socket.send("Message Number")
+}, 1000)
 
 new Vue({
   render: h => h(App),
